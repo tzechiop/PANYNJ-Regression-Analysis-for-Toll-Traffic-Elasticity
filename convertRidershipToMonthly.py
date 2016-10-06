4b7711abe9dbf8cc0a4c5a1b4457d041695b879d
thasegawa <takehase@umich.edu>
2016-10-06 12:53:25 -0400

    revert undelete

---
+++ convertRidershipToMonthly.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  6 09:29:50 2016
+
+This is a script that takes ridership data (presumably PATH) and sums it by
+month. The input data should have a column 'Year-Month' (unless specified otherwise)
+that can be used to group by month.
+
+To run this script on path data, use the following command:
+python covertRidershipToMonthly.py data\\path_ridership.xlsx -f 7 
+
+@author: thasegawa
+"""
+
+import argparse
+import os
+import pandas as pd
+
+print('Grouping data by month...')
+
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('infile')
+parser.add_argument('-o', '--outfile', help="specify the output file. default is input + '_monthly'")
+parser.add_argument('-f', '--firstcol', default=0, type=int, help="specifies the first column in the group of columns that should be preserved in the output")
+parser.add_argument('-l', '--lastcol', type=int, help="specifies the last column in the group of columns that should be preserved in the output")
+parser.add_argument('-c', '--groupcol', default='Year-Month', help="specifies the column to be used to group monthly data. Year and month should be split by '-'")
+args = vars(parser.parse_args())
+infile, outfile, firstcol, lastcol, groupcol = args['infile'], args['outfile'], args['firstcol'], args['lastcol'], args['groupcol']
+
+# If the outfile name was not specified, create default outfile name
+if outfile is None:
+    outfile, ext = os.path.splitext(infile)
+    outfile += '_monthly' + ext
+print('Input: %s' % infile)
+print('Output: %s' % outfile)
+
+# Read data
+data = pd.read_excel(infile)
+
+# Determine columns to keep in output file
+if lastcol is None:
+    lastcol = len(data.columns.values)
+keepcol_list = [column for column in data.ix[:,firstcol:lastcol]._get_numeric_data().columns.values if (column != groupcol)]
+
+# Sum ridership data by month
+monthly = data.groupby([groupcol]).sum()[keepcol_list]
+
+# Add back in date columns
+monthly[groupcol] = monthly.index
+monthly['Year'], monthly['Month'] = zip(*([int(y) for y in x.split('-')] for x in monthly.index))
+
+# Output monthly data
+monthly = monthly.sort_values(['Year', 'Month'])
+monthly[['Month', 'Year', groupcol] + keepcol_list].to_excel(outfile, index = False)
+
+print('Success!')
---
+++ createRegressInputs.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  6 09:29:50 2016
+
+This script is used to create the x and y data files to be used for regression.
+The script compiles columns from spreadsheets specified in 'colfile'. The column
+'groupcol' (default = 'Year-Month') is used to combine the columns from every
+spreadsheet, and thus should be included in every spreadsheet.
+The file colfile should have have the following fields: 'infile', 'column', 'xy'
+
+The line below provides an example of a command used to run this script.
+python createRegressInputs.py data\\regress_para\\regresscols_pathtotal_1.xlsx -o data\\regress_data
+
+
+@author: thasegawa
+"""
+
+import argparse
+import os
+import pandas as pd
+
+# Function to create output filename
+def createOutfilename(infile, add, outpath):
+    outfile, ext = os.path.splitext(infile)
+    outfile += add + ext
+    if outpath is not None:
+        outfile = os.path.join(outpath, os.path.basename(outfile))
+    return outfile
+    
+# Function to combine new x or y data
+def addNew(data, newdata, groupcol, columns):
+    if data is None:
+        data = pd.DataFrame(newdata[[groupcol] + columns])
+    else:
+        data = pd.merge(data, newdata[[groupcol] + columns], how = 'left', on = groupcol)
+    return data
+
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('colfile')
+parser.add_argument('-o', '--outpath', help="specify the output folder")
+parser.add_argument('-oy', '--outfiley', help="specify the output file. default is colfile + '_x' and colfile + '_y'")
+parser.add_argument('-ox', '--outfilex', help="specify the output file. default is colfile + '_x' and colfile + '_y'")
+parser.add_argument('-c', '--groupcol', default='Year-Month', help="specifies the column that specifies the time scale")
+parser.add_argument('-y', '--sumY', default=True, help="specifies if the Y data should be summed or not")
+args = vars(parser.parse_args())
+colfile, outpath, outfiley, outfilex, groupcol, sumY, removeNaN = args['colfile'], args['outpath'], args['outfiley'], args['outfilex'], args['groupcol'], args['sumY'], args['removeNaN']
+
+# If the outfile names were not specified, create defualt outfile names
+if outfiley is None:
+    outfiley = createOutfilename(colfile, '_y', outpath)
+if outfilex is None:
+    outfilex = createOutfilename(colfile, '_x', outpath)
+
+print('Column file: %s' % colfile)
+print('Outfile for y: %s' % outfiley)
+print('Outfile for x: %s' % outfilex)
+
+# Read column file
+coldata = pd.read_excel(colfile)
+infile_list = coldata['infile'].unique()
+
+# Compile Y data
+ydata = None
+xdata = None
+for infile in infile_list:
+    rdata = pd.read_excel(infile)
+
+    # Iterate through x and y data of each input spreadsheet    
+    subdata = coldata[coldata['infile'] == infile]    
+    xy_list = subdata['xy'].unique()
+    for xy in xy_list:
+        print('Compiling {0} data from {1}'.format(xy, infile))
+        idx = subdata['xy'] == xy
+        columns = list(subdata['column'][idx])
+        if xy == 'y':
+            ydata = addNew(ydata, rdata, groupcol, columns)
+        else:
+            xdata = addNew(xdata, rdata, groupcol, columns)
+                
+# Sum the Y columns if specified
+if sumY:
+    cols = [column for column in ydata.columns.values if column != groupcol]
+    ydata = pd.concat([ydata[groupcol], ydata[cols].sum(axis=1)], axis = 1)
+    ydata.columns = [groupcol, 'y']
+
+# Output files
+ydata.to_excel(outfiley, index = False)
+xdata.to_excel(outfilex, index = False)
+print('Success!')
